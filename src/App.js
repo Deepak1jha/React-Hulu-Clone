6bbc6fa32a33b6370eadb78193f895d718e12290
Deepak1jha <ddjha171@gmail.com>
2020-09-02 00:55:05 +0530

    Navbar complete

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import './App.css';
 import Header from "./components/header/Header";
+import Navbar from "./components/navbar/Navbar";
 
 function App() {
   return (
     <div className="app">
       <Header/>
-
+      <Navbar/>
     </div>
   );
 }
---
+++ src/components/navbar/Navbar.js
@@ -0,0 +1,20 @@
+import React from "react";
+import "../navbar/Navbar.css";
+
+export default function Navbar() {
+  return (
+    <div className="navbar">
+      <h2>Trending</h2>
+      <h2>Top-Related</h2>
+      <h2>Action</h2>
+      <h2>Comedy</h2>
+      <h2>Horror</h2>
+      <h2>Romance</h2>
+      <h2>Mystery</h2>
+      <h2>Bollywood</h2>
+      <h2>Crime</h2>
+      <h2>Sports</h2>
+      <h2>Biography</h2>
+    </div>
+  )
+}
