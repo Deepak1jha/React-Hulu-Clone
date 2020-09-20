9e6c9f47dd00740ca76f9fc673b673202c8c74f3
Deepak1jha <ddjha171@gmail.com>
2020-09-20 18:15:33 +0530

    Updates

---
+++ precache-manifest.3893a7ae336f5410cc879340be3aeab8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f772b03cbf9311879e9dd59bda00abf1",
+    "url": "/React-Hulu-Clone/index.html"
+  },
+  {
+    "revision": "d6eb786e5d60e4b55a2f",
+    "url": "/React-Hulu-Clone/static/css/main.2b9a83c3.chunk.css"
+  },
+  {
+    "revision": "01951fa8e7d0a92061eb",
+    "url": "/React-Hulu-Clone/static/js/2.1598c791.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/React-Hulu-Clone/static/js/2.1598c791.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d6eb786e5d60e4b55a2f",
+    "url": "/React-Hulu-Clone/static/js/main.73a0124b.chunk.js"
+  },
+  {
+    "revision": "df07b5770e5f6be766ca",
+    "url": "/React-Hulu-Clone/static/js/runtime-main.bbc79b16.js"
+  }
+]);
