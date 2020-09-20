c439440aac8b21ae59dafa663c4677742e33b49c
Deepak1jha <ddjha171@gmail.com>
2020-09-20 18:14:54 +0530

    Deploy

--- src/api/request/request.js
+++ src/api/request/request.js
@@ -3,5 +3,12 @@ export default {
   fetchTrending: `/trending/all/week?api_key=${API_KEY}&language=en-US`,
   fetchTopRated: `/movie/top_rated?api_key=${API_KEY}&language=en-US`,
   fetchUpComing: `/movie/upcoming?api_key=${API_KEY}&language=en-US`,
-  fetchPopular: `/movie/popular?api_key=${API_KEY}&language=en-US`
+  fetchPopular: `/movie/popular?api_key=${API_KEY}&language=en-US`,
+  fetchActionMovies: `/discover/movie?api_key=${API_KEY}&with_genres=28`,
+  fetchComedyMovies: `/discover/movie?api_key=${API_KEY}&with_genres=35`,
+  fetchHorrorMovies: `/discover/movie?api_key=${API_KEY}&with_genres=27`,
+  fetchRomanceMovies: `/discover/movie?api_key=${API_KEY}&with_genres=10749`,
+  fetchMysteryMovies: `/discover/movie?api_key=${API_KEY}&with_genres=9648`,
+  fetchSciFiMovies: `/discover/movie?api_key=${API_KEY}&with_genres=878`,
+
 }
--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -7,15 +7,12 @@ export default function Navbar({setSelectedOption}) {
     <div className="navbar">
       <h2 onClick={()=>setSelectedOption(request.fetchTrending)}>Trending</h2>
       <h2 onClick={()=>setSelectedOption(request.fetchTopRated)}>Top-Related</h2>
-      <h2>Action</h2>
-      <h2>Comedy</h2>
-      <h2>Horror</h2>
-      <h2>Romance</h2>
-      <h2>Mystery</h2>
-      <h2>Bollywood</h2>
-      <h2>Crime</h2>
-      <h2>Sports</h2>
-      <h2>Biography</h2>
+      <h2 onClick={()=>setSelectedOption(request.fetchActionMovies)}>Action</h2>
+      <h2 onClick={()=>setSelectedOption(request.fetchComedyMovies)}>Comedy</h2>
+      <h2 onClick={()=>setSelectedOption(request.fetchHorrorMovies)}>Horror</h2>
+      <h2 onClick={()=>setSelectedOption(request.fetchRomanceMovies)}>Romance</h2>
+      <h2 onClick={()=>setSelectedOption(request.fetchMysteryMovies)}>Mystery</h2>
+      <h2 onClick={()=>setSelectedOption(request.fetchSciFiMovies)}>Sci fi</h2>
     </div>
   )
 }
